d7a76c61829afdda53817dad77e4bd17f91e241b
Nouran <nourans.96@yahoo.com>
2020-03-26 21:05:10 +0200

    Showed user details in post

--- database/migrations/2020_03_26_184620_add_user_id_to_posts.php
+++ database/migrations/2020_03_26_184620_add_user_id_to_posts.php
@@ -14,7 +14,7 @@ class AddUserIdToPosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('user_id')->nullable();
         });
     }
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,9 +7,18 @@
         <div class="card-header">Post Info</div>
         <div class="card-body">
             <h5 class="card-title"><strong>Title: </strong>{{$post->title}}</h5>
-            <p class=" small card-text">{{$post->created_at->toDateString()}}</p>
+            <!-- <p class=" small card-text">{{$post->created_at->toDateString()}}</p> -->
             <h5><strong>Description:</strong> </h5>
             <p class="card-text">{{$post->description}}</p>
+        </div>
+    </div>
+
+    <div class="card mb-3" style="max-width: 18rem;">
+        <div class="card-header">Post Creator Info</div>
+        <div class="card-body">
+            <h5 class="card-title"><strong>Name: </strong>{{$post->user->name}}</h5>
+            <p class="card-text"><strong>Email: </strong>{{$post->user->email}}</p>
+            <p class="card-text"><strong>Created At: </strong>{{$post->created_at}}</p>
         </div>
     </div>
 @endsection
