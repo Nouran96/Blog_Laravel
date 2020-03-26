f63158c2c3d440ba0fa4fb9f25bf8167cd6b3754
Nouran <nourans.96@yahoo.com>
2020-03-26 20:01:09 +0200

    Showed all posts in browser

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        $posts = Post::all();
+
+        return view('posts.index', [
+            'posts'=> $posts
+        ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,31 +5,23 @@
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Created At</th>
+      <th scope="col">Actions</th>
     </tr>
   </thead>
   <tbody>
+  @foreach($posts as $post)
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">{{$post->id}}</th>
+      <td>{{$post->title}}</td>
+      <td>{{$post->description}}</td>
+      <td>{{$post->created_at}}</td>
+      <td><a href="#" class="btn btn-primary btn-sm">View Details</a></td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+  @endforeach
   </tbody>
 </table>
 
